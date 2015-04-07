d13b99fd89ae53e72c0be170ab95a558227c9d5e
Igor Gaponov <jiminy96@gmail.com>
2015-04-07 10:00:17 +0200

    added supports tests for Gateway

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -17,4 +17,29 @@ class GatewayTest extends GatewayTestCase
 
         $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
     }
+
+    public function testSupportsEnrollment()
+    {
+        $this->assertInstanceOf('Omnipay\Common\Message\RequestInterface', $this->gateway->enrollment());
+    }
+
+    public function testSupportsPreauthorization()
+    {
+        $this->assertInstanceOf('Omnipay\Common\Message\RequestInterface', $this->gateway->preauthorization());
+    }
+
+    public function testSupportsReversal()
+    {
+        $this->assertInstanceOf('Omnipay\Common\Message\RequestInterface', $this->gateway->reversal());
+    }
+
+    public function testSupportsQuery()
+    {
+        $this->assertInstanceOf('Omnipay\Common\Message\RequestInterface', $this->gateway->query());
+    }
+
+    public function testSupportsBookBack()
+    {
+        $this->assertInstanceOf('Omnipay\Common\Message\RequestInterface', $this->gateway->bookBack());
+    }
 }
