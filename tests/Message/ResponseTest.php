97fc47225c761fcec56faa6d7d023070c6458a13
Igor Gaponov <jiminy96@gmail.com>
2015-04-10 09:19:42 +0200

    added Response tests

---
+++ tests/Message/ResponseTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Omnipay\Wirecard\Message;
+
+use PHPUnit_Framework_TestCase;
+
+class ResponseTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @param $method
+     * @return \PHPUnit_Framework_MockObject_MockObject|Response
+     */
+    protected function getResponseMock($method)
+    {
+        $data = $this->getMockBuilder('\Wirecard\Element\Response')
+            ->setMethods([$method])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $data->expects($this->once())->method($method);
+
+        $response = $this->getMockBuilder('\Omnipay\Wirecard\Message\Response')
+            ->setMethods(['getData'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $response->expects($this->any())->method('getData')->will($this->returnValue($data));
+
+        return $response;
+    }
+
+    public function testIsRedirectCallsDataMethod()
+    {
+        $response = $this->getResponseMock('isRedirect');
+        $response->isRedirect();
+    }
+
+    public function testGetMessageCallsDataMethod()
+    {
+        $response = $this->getResponseMock('getMessage');
+        $response->getMessage();
+    }
+
+    public function testGetTransactionReferenceCallsDataMethod()
+    {
+        $response = $this->getResponseMock('getProcessingGuWid');
+        $response->getTransactionReference();
+    }
+
+    public function testGetRedirectUrlCallsDataMethod()
+    {
+        $response = $this->getResponseMock('getUrl');
+        $response->getRedirectUrl();
+    }
+
+    public function testIsSuccessfulCallsDataMethod()
+    {
+        $response = $this->getResponseMock('isSuccessful');
+        $response->isSuccessful();
+    }
+
+    public function testGetTokenCallsDataMethod()
+    {
+        $response = $this->getResponseMock('getToken');
+        $response->getToken();
+    }
+}
